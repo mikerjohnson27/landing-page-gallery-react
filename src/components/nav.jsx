656a776c999fb7c91ca5f60c36814f47f3bafd70
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-10 21:15:23 -0400

    adding react-bootstap to dependancies.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,10 +8,19 @@ class Nav extends Component {
 
     render() { 
         return (
-        <div>
-            <span></span>
-            <button>Word</button>
-        </div>
+<Nav variant="pills" defaultActiveKey="/home">
+    <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+        Disabled
+        </Nav.Link>
+    </Nav.Item>
+</Nav>
         );
     }
 }
