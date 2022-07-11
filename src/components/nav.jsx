c227d778abdf9997de8d64eb6ec3bc4cef312d78
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-10 20:15:05 -0400

    adding home.jsx and revisions to index.js

---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+
+    state = {
+        count: 0,
+    };
+
+    render() { 
+        return (
+        <div>
+            <span></span>
+            <button>Word</button>
+        </div>
+        );
+    }
+}
+ 
+export default Nav
