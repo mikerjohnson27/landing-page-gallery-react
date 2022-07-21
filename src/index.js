e315683c76a66bf3d83dea8479b38a92185a6076
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-21 17:10:14 -0400

    Revizing To New React 18.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import NavTop from './Nav/NavTop';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Nav/NavTop.css'
 
+const container = document.getElementById('root');
+const root = createRoot(container)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <NavTop />
-  </React.StrictMode>
-);
+root.render(<NavTop />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
