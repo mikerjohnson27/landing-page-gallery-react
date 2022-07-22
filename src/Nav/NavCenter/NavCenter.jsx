4d5e3e8e11b746da638f51d28bacf1305851ba74
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-21 21:37:20 -0400

    Adding Heading To Nav

---
+++ src/Nav/NavCenter/NavCenter.jsx
@@ -0,0 +1,14 @@
+import Nav from 'react-bootstrap/Nav';
+import './NavCenter.css'
+
+
+function NavCenter() {
+    return (
+        <Nav>
+            <Nav.Item className='navItemdiv'>
+                <p className="mt-4 mb-4 navTitle">Landing Pages For Sale</p>
+            </Nav.Item>
+        </Nav>
+    );
+}
+export default NavCenter
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client'
-import NavTop from './Nav/NavTop';
+import NavTop from './Nav/NavTop/NavTop';
+import NavCenter from './Nav/NavCenter/NavCenter'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Nav/NavTop.css'
+import './Nav/NavTop/NavTop.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container)
 
-root.render(<NavTop />);
+root.render(
+    <>
+    <NavCenter />
+        <NavTop />
+    </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
