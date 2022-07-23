642b372f9b8818f428e2eae5b1e6940afba0447b
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-23 15:22:29 -0400

    File organization.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+
+//React Dependencies
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client'
-import NavTop from './Nav/NavTop/NavTop'
-import NavCenter from './Nav/NavCenter/NavCenter'
-import Product from './home/home'
+
+//React JSX files.
+import NavTop from './Home/Nav/NavTop/NavTop'
+import NavCenter from './Home/Nav/NavCenter/NavCenter'
+import Product from './Home/product/product'
+
+//css files
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './Nav/NavTop/NavTop.css'
-import './home/home.css'
+import './Home/Nav/NavTop/NavTop.css'
 
 
 const container = document.getElementById('root');
