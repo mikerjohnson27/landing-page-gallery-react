c5aab8a40bd18cf57455e6af6c56a0fd4427c531
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-23 19:25:50 -0400

    center items on the card.

--- src/Home/product/product.jsx
+++ src/Home/product/product.jsx
@@ -9,8 +9,8 @@ function Product() {
     <>
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="https://dummyimage.com/180x100/737373/ffffff.jpg&text=Place+Holder" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+        <Card.Body align="center">
+          <Card.Title align="center">Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
